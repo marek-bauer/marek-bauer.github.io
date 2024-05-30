3e460934fd36b01b388bb2a5146c21bd6d816d7b
Marek Bauer <marekbauer07@gmail.com>
2024-05-30 14:40:53 +0200

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent ],
+  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
